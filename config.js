01b32f55489904c150b6205a004d5f4039e5a43f
Sebastian <xugnad@gmail.com>
2016-02-19 23:10:59 +0100

    config fixed

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 /*Config data*/
+// config = {
+//   mongoConnect : 'mongodb://heroku_w3wclq1l:8rij1ncpbl533qq880pegsnkb4@ds011288.mongolab.com:11288/heroku_w3wclq1l'
+// };
+//local test
 config = {
-  mongoConnect = 'mongodb://heroku_w3wclq1l@ds011288.mongolab.com:11288/heroku_w3wclq1l'
+  mongoConnect : 'mongodb://localhost/domains-crawler-xugnad'
 };
+
+
 
 module.exports = config;
